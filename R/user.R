86dbb324fbb8fdbf877e668af319cc4eac304c78
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2020-01-12 21:48:13 -0500

    Add csr_dataset() and csr_database() and test code

--- R/user.R
+++ R/user.R
@@ -1,4 +1,4 @@
-# User-side convenience functions
+# User-side functions
 
 #' Extract one table (\code{description}, \code{data}, etc.) of all datasets
 #'
@@ -17,6 +17,37 @@ csr_table <- function(all, table) {
   }
 
   rbind_list(lapply(all, extract, table = table))
+}
+
+#' Return an overview of available datasets
+#'
+#' @param all All data, a list of lists
+#'
+#' @return A `data.frame` or `tibble` with data about the constitutent datasets of COSORE.
+#' @export
+#' @note This is a convenience function, as it simply calls
+#' \code{\link{csr_table}} to return the joined \code{description} tables.
+csr_database <- function(all) {
+  csr_table(all, "description")
+}
+
+#' Return an individual dataset
+#'
+#' @param all All data, a list of lists
+#' @param dataset Name of dataset, character
+#' @return A list with (at least) elements:
+#' \item{description}{Contents of \code{DESCRIPTION.txt} file}
+#' \item{contributors}{Contents of \code{CONTRIBUTORS.txt} file}
+#' \item{ports}{Contents of \code{PORTS.txt} file}
+#' \item{data}{Continuous soil respiration data, parsed into a \code{data.frame}}
+#' \item{diagnostics}{Diagnostics on the data parsing and QC process}
+#' \item{ancillary}{Ancillary site information}
+#' @export
+csr_dataset <- function(all, dataset) {
+  if(!dataset %in% names(all)) {
+    stop("Unknown dataset")
+  }
+  all[[dataset]]
 }
 
 
--- tests/testthat/test_user.R
+++ tests/testthat/test_user.R
@@ -12,3 +12,16 @@ test_that("user utilities work", {
   expect_is(x, "data.frame")
   expect_identical(nrow(x), nrow(cars) * length(all_data))
 })
+
+
+test_that("csr_dataset", {
+
+  ds1 <- list(data = cars, dataset_name = "DS1")
+  all_data <- list(DS1 = ds1,
+                   DS2 = list(data = cars, dataset_name = "DS2"),
+                   DS3 = list(data = cars, dataset_name = "DS3"))
+
+  expect_error(csr_dataset(all_data, "DS4"))
+  expect_identical(csr_dataset(all_data, "DS1"), ds1)
+})
+
